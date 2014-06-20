caf96c4fb64928ad05ad67673953fe7c0badbf90
Deane Barker <deane@blendinteractive.com>
2014-06-20 15:05:15 -0500

    Added category

---
+++ InsightlyApi/InsightlyObjects/InsightlyCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace InsightlyApi.InsightlyObjects
+{
+    [InsightlyTableMapping(ObjectName = "Category", TableName = "Categories")]
+    public class InsightlyCategory : InsightlyObject
+    {
+        public InsightlyCategory(XmlElement xml) : base(xml)
+        {
+
+        }
+
+        public override string Id
+        {
+            get { return GetValue(".//{0}CATEGORY_ID"); }
+        }
+
+        [InsightlyColumnMapping]
+        public string Name
+        {
+            get { return GetValue(".//{0}CATEGORY_NAME"); }
+        }
+    }
+}
