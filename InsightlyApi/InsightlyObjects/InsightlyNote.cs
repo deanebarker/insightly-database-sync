a793a03882d44e482bcc3d0824c6954ce32a6505
Deane Barker <deane@blendinteractive.com>
2014-11-25 11:31:06 -0600

    Added Title and ContactId properties to the Note object.

---
+++ InsightlyApi/InsightlyObjects/InsightlyNote.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace InsightlyApi.InsightlyObjects
+{
+    [InsightlyTableMapping(ObjectName = "Note", TableName = "Notes")]
+    public class InsightlyNote : InsightlyObject
+    {
+        public InsightlyNote(XmlElement xml)
+            : base(xml)
+        {
+
+        }
+
+        public override string Id
+        {
+            get { return GetValue(".//{0}NOTE_ID"); }
+        }
+
+        [InsightlyColumnMapping]
+        public string Title
+        {
+            get { return GetValue(".//{0}TITLE"); }
+        }
+
+
+        [InsightlyColumnMapping]
+        public string Body
+        {
+            get { return GetValue(".//{0}BODY"); }
+        }
+
+        [InsightlyColumnMapping]
+        public string OpportunityId
+        {
+            get { return GetValue(".//{0}OPPORTUNITY_ID"); }
+        }
+
+        [InsightlyColumnMapping]
+        public string ContactId
+        {
+            get { return GetValue(".//{0}CONTACT_ID"); }
+        }
+
+        [InsightlyColumnMapping]
+        public string OwnerId
+        {
+            get { return GetValue(".//{0}OWNER_USER_ID"); }
+        }
+
+        [InsightlyColumnMapping]
+        public string Created
+        {
+            get { return GetValue(".//{0}DATE_CREATED_UTC"); }
+        }
+    }
+}
